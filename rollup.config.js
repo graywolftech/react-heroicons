12cdb048a6708f4ffddede8e5470ffb967ba16c4
Jacob Smith <jacob.smith@unb.ca>
2020-05-17 09:52:21 -0300

    now bundling d.ts file

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,18 @@
 import typescript from '@rollup/plugin-typescript';
+import dts from "rollup-plugin-dts";
 
-export default {
-  input: 'index.ts',
-  output: {
-    dir: 'dist',
-    format: 'module'
+export default [
+  {
+    input: 'index.ts',
+    output: {
+      dir: 'dist',
+      format: 'module'
+    },
+    plugins: [typescript()]
   },
-  plugins: [typescript()]
-};
+  {
+    input: "dist/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+];
